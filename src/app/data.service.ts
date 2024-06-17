f30952a4eb164c455c83b3a4afc50ecf83243a74
David <saumet.david@orange.fr>
2024-06-17 11:36:56 +0200

    add openstreet

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,17 +7,26 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class DataService {
-  private jsonUrl = 'assets/datas.json';
+  private jsonUrl = 'assets/datas.json'; // URL vers votre fichier JSON, à adapter si nécessaire
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  public getData(): Observable<any> {
-    return this.http.get(this.jsonUrl);
+  public getData(): Observable<any[]> {
+    return this.http.get<any[]>(this.jsonUrl);
   }
 
   public getArtisansOfTheMonth(): Observable<any[]> {
     return this.getData().pipe(
-      map((artisans: any[]) => artisans.filter(artisan => artisan.top))
+      map((artisans: any[]) => artisans.filter(artisan => artisan.top)),
+      map((filteredArtisans: any[]) => {
+        return filteredArtisans.map(artisan => ({
+          ...artisan,
+          coordinates: {
+            latitude: artisan.coordinates.latitude || 0,  // Assurez-vous que les coordonnées sont bien gérées
+            longitude: artisan.coordinates.longitude || 0  // Assurez-vous que les coordonnées sont bien gérées
+          }
+        }));
+      })
     );
   }
 
@@ -27,5 +36,9 @@ export class DataService {
     );
   }
 }
+
+
+
+
 
 
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,29 @@ import { DataService } from '../../data.service';
 export class HomeComponent implements OnInit {
   artisansOfTheMonth: any[] = [];
 
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
     this.dataService.getArtisansOfTheMonth().subscribe(data => {
       this.artisansOfTheMonth = data;
+      console.log('Artisans of the month:', this.artisansOfTheMonth);
     });
   }
+
+  sanitizeMapUrl(latitude: number, longitude: number): SafeResourceUrl {
+    const url = `https://www.openstreetmap.org/export/embed.html?bbox=${longitude - 0.01},${latitude - 0.01},${longitude + 0.01},${latitude + 0.01}&amp;layer=mapnik`;
+    console.log('Generated URL:', url); // Vérifiez l'URL générée
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+  
 }
+
+
+
+
+
+
 
