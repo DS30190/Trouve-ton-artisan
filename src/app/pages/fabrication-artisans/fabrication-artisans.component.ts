c343d3af5657f9b3109a45ca3e1cdc546e21b391
David <saumet.david@orange.fr>
2024-06-17 12:56:21 +0200

    add fab

--- src/app/pages/fabrication-artisans/fabrication-artisans.component.ts
+++ src/app/pages/fabrication-artisans/fabrication-artisans.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../data.service';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-fabrication-artisans',
@@ -9,12 +10,42 @@ import { DataService } from '../../data.service';
 export class FabricationArtisansComponent implements OnInit {
   artisans: any[] = [];
 
-  constructor(private dataService: DataService) { }
+  constructor(
+    private dataService: DataService,
+    private sanitizer: DomSanitizer
+  ) { }
 
   ngOnInit(): void {
     this.dataService.getData().subscribe(data => {
       this.artisans = data.filter((artisan: any) => artisan.category === 'Fabrication');
     });
+  }
+  sanitizeMapUrl(latitude: number, longitude: number): SafeResourceUrl {
+    const url = `https://www.openstreetmap.org/export/embed.html?bbox=${longitude - 0.01},${latitude - 0.01},${longitude + 0.01},${latitude + 0.01}&amp;layer=mapnik`;
+    console.log('Generated URL:', url); // Vérifiez l'URL générée
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+  
+  getStarClasses(note: number): string[] {
+    const starClasses = [];
+    const fullStars = Math.floor(note);
+    const remainingStars = note - fullStars;
+    const hasHalfStar = remainingStars >= 0.5;
+    const hasQuarterStar = remainingStars >= 0.25 && remainingStars < 0.75;
+  
+    for (let i = 0; i < 5; i++) {
+      if (i < fullStars) {
+        starClasses.push('fa fa-star');
+      } else if (hasHalfStar && i === fullStars) {
+        starClasses.push('fa fa-star-half-o');
+      } else if (hasQuarterStar && i === fullStars) {
+        starClasses.push('fa fa-star-quarter-o');
+      } else {
+        starClasses.push('fa fa-star-o');
+      }
+    }
+  
+    return starClasses;
   }
 }
 
